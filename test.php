142409684ad84b97d6bf19749c5294ce4f8cc3ca
Os1o <seblverde19@outlook.com>
2025-10-02 10:38:59 -0600

    Update

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test"; // Change to your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection successful";
+
+$conn->close();
+?>
